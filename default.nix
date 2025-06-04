e5f9eb71a1f18750764d1f6f1d900f661016272b
Dark-LYNN <botdevlynn@gmail.com>
2025-06-04 21:33:06 +0200

    testing

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-pkgs: pkgs.stdenv.mkDerivation rec {
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.stdenv.mkDerivation rec {
   pname = "jdownloader";
   version = "2";
   unpackPhase = ":";
@@ -9,27 +12,27 @@ pkgs: pkgs.stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ pkgs.makeWrapper pkgs.jre ];
+  homeDir = builtins.getEnv "HOME";
 
-installPhase = ''
-  mkdir -p $out/libexec/jdownloader
-  cp $src $out/libexec/jdownloader/JDownloader.jar
+  installPhase = ''
+    mkdir -p $out/libexec/jdownloader
+    cp $src $out/libexec/jdownloader/JDownloader.jar
 
-  mkdir -p $out/bin
-  cat > $out/bin/jdownloader <<'EOF'
+    mkdir -p $out/bin
+    cat > $out/bin/jdownloader <<EOF
 #!/bin/sh
 
-homeDir="$HOME"
-mkdir -p "$homeDir/.config/JDownloader/cfg" "$homeDir/.config/JDownloader/logs"
-cd "$homeDir/.config/JDownloader"
+mkdir -p "${homeDir}/.config/JDownloader/cfg" "${homeDir}/.config/JDownloader/logs"
+cd ${homeDir}/.config/JDownloader
 
-if [ ! -e "$homeDir/.config/JDownloader/JDownloader.jar" ]; then
-  cp "$out/libexec/jdownloader/JDownloader.jar" "$homeDir/.config/JDownloader/JDownloader.jar"
-  chmod u+w "$homeDir/.config/JDownloader/JDownloader.jar"
+if [ ! -e "${homeDir}/.config/JDownloader/JDownloader.jar" ]; then
+  cp "$out/libexec/jdownloader/JDownloader.jar" "${homeDir}/.config/JDownloader/JDownloader.jar"
+  chmod u+w ${homeDir}/.config/JDownloader/JDownloader.jar
 fi
 
-export JD_CONFIG_DIR="$homeDir/.config/JDownloader"
+export JD_CONFIG_DIR="${homeDir}/.config/JDownloader"
 
-exec ${pkgs.jre}/bin/java -jar "$homeDir/.config/JDownloader/JDownloader.jar" "$@"
+exec ${pkgs.jre}/bin/java -jar "${homeDir}/.config/JDownloader/JDownloader.jar" "$@"
 EOF
 
   chmod +x $out/bin/jdownloader
@@ -45,8 +48,7 @@ Terminal=false
 Type=Application
 Categories=Network;Utility;
 EOF
-'';
-
+  '';
 
   meta = with pkgs.lib; {
     description = "Download manager for file hosting services";
